0466864714a70ebd292deb8fe7822e9acee5e93c
Cristian Franco <cristian.franko.cf@gmail.com>
2025-09-23 01:38:09 -0300

    feat: Implement product catalog structure and display

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\ShowProducts;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ShowProducts::class);
